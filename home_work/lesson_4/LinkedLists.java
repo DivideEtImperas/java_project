68ff2389842e3389342b264222682070e18704f3
Eduard Golyshev <tula71ya@mail.ru>
2023-05-07 13:03:02 +0300

    modified LinkedLists

--- home_work/lesson_4/LinkedLists.java
+++ home_work/lesson_4/LinkedLists.java
@@ -1,61 +1,100 @@
 package Java_project.home_work.lesson_4;
-/*Пусть дан LinkedList с несколькими элементами. Реализуйте метод, который вернёет «перевёрнутый» список.
+/*
+ * Пусть дан LinkedList с несколькими элементами. Реализуйте метод, который вернёет «перевёрнутый» список.
 
- *Реализуйте очередь с помощью LinkedList со следующими методами:
+ * Реализуйте очередь с помощью LinkedList со следующими методами:
 • enqueue() — помещает элемент в конец очереди,
 • dequeue() — возвращает первый элемент из очереди и удаляет его,
 • first() — возвращает первый элемент из очереди, не удаляя.
  */
-import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Random;
 
 public class LinkedLists {
   public static void main(String[] args) {
-          
-    ArrayDeque<String> states = new ArrayDeque<String>();
-    // стандартное добавление элементов
-    states.add("Germany");
-    states.addFirst("France"); // добавляем элемент в самое начало
-    states.push("Great Britain"); // добавляем элемент в самое начало
-    states.addLast("Spain"); // добавляем элемент в конец коллекции
-    states.add("Italy");
+    
+    LinkedList<Integer> list = new LinkedList<>();
+    Queue<Integer> queue = new LinkedList<>();
+    Deque<Integer> deque = new LinkedList<>();
+    
+    fillList(list);
+    System.out.println(printList(list));
+    System.out.println();
+    System.out.println("Лист до разворота: " + list);
+    list = reversLinkedList(list);
+    System.out.println("Лист после разворота: " + list);
+    System.out.println("Добавляем эллемент в конец списка");
+    enQueue(queue);
+    System.out.println("Queue: " + queue);
+    System.out.println("Удаляем первый эллемент из списка");
+    firstRemoveDeque(deque);
+    System.out.println("Deque: " + deque);
+  
+    
+    
+  
+  }
+  // Метод заполнения листа рандомными числами
+  public static LinkedList<Integer> fillList(LinkedList<Integer> list) {
+    Random rand = new Random();
+    for (int i = 1; i < 10 ; i++) {
       
-    // получаем первый элемент без удаления
-    String sFirst = states.getFirst();
-    System.out.println(sFirst);     // Great Britain
-    // получаем последний элемент без удаления
-    String sLast = states.getLast();
-    System.out.println(sLast);      // Italy
-     
-    System.out.printf("Queue size: %d \n", states.size());  // 5
-     
-    // перебор коллекции        
-    while(states.peek()!=null){
-        // извлечение c начала
-        System.out.println(states.pop());
-    }
-      
-     // очередь из объектов Person
-    ArrayDeque<Person> people = new ArrayDeque<Person>();
-    people.addFirst(new Person("Tom"));
-    people.addLast(new Person("Nick"));
-    // перебор без извлечения
-    for(Person p : people){
-      
-        System.out.println(p.getName());
+       list.add(rand.nextInt(20));    
        
     }
-}
-}
-class Person{
-  
-private String name;
-
-public Person(String value){
+    return list;
     
-    name=value;
-}
-String getName(){return name;}
+    
+  }
+  // Метод печати листа
+  public static LinkedList<Integer> printList(LinkedList<Integer> list) {
+    return list;
+    
+  }
+  /*
+   * Пусть дан LinkedList с несколькими элементами. Реализуйте метод, который вернёет «перевёрнутый» список.
+   */
+  // Метод который разворачивает лист
+  public static LinkedList<Integer> reversLinkedList(LinkedList<Integer>list) {
+    LinkedList<Integer> revList = new LinkedList<Integer>();
+      for (int i = list.size()-1; i>=0;i--){
+        revList.add(list.get(i));
+      }    
+      return revList;
+  }
+  /*
+   * Реализуйте очередь с помощью LinkedList со следующими методами:
+      • enqueue() — помещает элемент в конец очереди,
+      • dequeue() — возвращает первый элемент из очереди и удаляет его,
+      • first() — возвращает первый элемент из очереди, не удаляя.
+   */
 
+  // Метод queue добавление в конец списка
+  public static Queue<Integer> enQueue(Queue<Integer> queue) {
+    Random rand = new Random();
+    for (int i = 0; i < 6; i++) {
+      queue.add(rand.nextInt(1,10));
+      
+      
+    }
+    System.out.println("Начальный список: " + queue);
+    queue.add(5); // Здесь делаем добавление в конец списка
+    return queue;
+      
+  }
+  public static Deque<Integer> firstRemoveDeque(Deque<Integer> deque) {
+    Random rand = new Random();
+    for (int i = 0; i < 6; i++) {
+      deque.add(rand.nextInt(1,10));
+    }
+    System.out.println("Начальный список: " + deque);
+    deque.removeFirst();
+    System.out.println("Возвращает первый эллемент не удаляя его из списка: " + deque.getFirst());
+    return deque;
+    
+  }
 
 }
 
